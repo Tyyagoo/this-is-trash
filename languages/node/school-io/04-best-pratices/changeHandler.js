47a5fd408674b32bc907f285b9d47841d2fb3963
Tyyagoo <tyyagoosantos@gmail.com>
2021-11-22 14:37:03 -0300

    tdd
    the design of these tests is just stupid,
    why the fuck would i want to keep the "coins"
    in alphabetical order and not in order of magnitude?

--- languages/node/school-io/04-best-pratices/changeHandler.js
+++ languages/node/school-io/04-best-pratices/changeHandler.js
@@ -2,21 +2,39 @@ function throwException(coinType) {
   throw new Error("Unrecognized coin " + coinType);
 }
 
-module.exports = {
-  getAmount: function (coinType) {
-    // COINS:
-    // [p]enny
-    // [n]ickel
-    // [d]ime
-    // [q]uarter
+function getAmount(coinType) {
+  // [p]enny - [n]ickel - [d]ime - [q]uarter
+  return (
+    {
+      p: 1,
+      n: 5,
+      d: 10,
+      q: 25,
+    }[coinType] ?? throwException(coinType) // this looks worst than sw1tch LOL
+  );
+}
 
-    return (
-      {
-        p: 1,
-        n: 5,
-        d: 10,
-        q: 25,
-      }[coinType] ?? throwException(coinType) // this looks worst than sw1tch LOL
-    );
+function conversionStep(amount, acc) {
+  if (amount <= 0) return acc;
+  const currentConversion = {
+    // welcome to javascript
+    [amount >= getAmount("p")]: "p",
+    [amount >= getAmount("n")]: "n",
+    [amount >= getAmount("d")]: "d",
+    [amount >= getAmount("q")]: "q",
+  }.true;
+
+  console.log(`${amount} -> ${currentConversion}`);
+
+  return conversionStep(amount - getAmount(currentConversion), [
+    ...acc,
+    currentConversion,
+  ]);
+}
+
+module.exports = {
+  getAmount,
+  convertToChange: function (amount) {
+    return conversionStep(amount, []);
   },
 };
