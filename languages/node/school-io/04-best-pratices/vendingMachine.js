c69c0a63437fe5b55bcf27c9904f88c50fe86a0f
Tyyagoo <tyyagoosantos@gmail.com>
2021-11-22 14:57:53 -0300

    full impl

--- languages/node/school-io/04-best-pratices/vendingMachine.js
+++ languages/node/school-io/04-best-pratices/vendingMachine.js
@@ -2,26 +2,7 @@ var balanceManager = require("./balanceManager");
 var changeHandler = require("./changeHandler");
 var productInventory = require("./productInventory");
 
-var products = [
-  {
-    name: "Skittles",
-    price: 85,
-    id: "A1",
-  },
-];
-
 module.exports = {
-  getProducts: function () {
-    return products;
-  },
-
-  getProduct: function (productId) {
-    var product = products.find(function (p) {
-      return p.id === productId;
-    });
-    return product;
-  },
-
   insertCoin: function (coinType) {
     var value = changeHandler.getAmount(coinType);
     balanceManager.increaseBalance(value);
@@ -30,12 +11,14 @@ module.exports = {
   releaseChange: function () {
     var currentBalance = balanceManager.getBalance();
     balanceManager.decreaseBalance(currentBalance);
-    return this.convertToChange(currentBalance);
+    return changeHandler.convertToChange(currentBalance);
   },
 
   vendProduct: function (productId) {
-    var product = this.getProduct(productId);
+    var product = productInventory.getProduct(productId);
     balanceManager.decreaseBalance(product.price);
     return product;
   },
+
+  getProducts: productInventory.getProducts,
 };
