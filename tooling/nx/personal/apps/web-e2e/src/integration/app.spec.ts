ecf25574ed34dea78f9a8df36493c51d0ad5ae4f
Tyyagoo <tyyagoosantos@gmail.com>
2021-12-10 09:21:07 -0300

    (e2e): cypress testing on web & storybook

--- tooling/nx/personal/apps/web-e2e/src/integration/app.spec.ts
+++ tooling/nx/personal/apps/web-e2e/src/integration/app.spec.ts
@@ -1,13 +1,14 @@
 import { getGreeting } from '../support/app.po';
 
 describe('web', () => {
-  beforeEach(() => cy.visit('/'));
+  beforeEach(() => cy.visit('/articles/lorem-ipsum'));
 
   it('should display welcome message', () => {
     // Custom command example, see `../support/commands.ts` file
-    cy.login('my-email@something.com', 'myPassword');
 
     // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains('Welcome to web!');
+    getGreeting().contains('What is lorem ipsum?');
+
+    cy.get('iframe').should('be.visible');
   });
 });
